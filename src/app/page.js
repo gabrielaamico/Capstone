1e5964691b7d9d2945a55bf313f74cea4ac0c3c2
Gabriela Mico <gabriela.mico3@gmail.com>
2025-03-11 14:53:33 +0200

    Meta tags and OGP

--- src/app/page.js
+++ src/app/page.js
@@ -12,6 +12,9 @@ export default function Home() {
           name="description"
           content="Enjoy delicious italian mediteranean meals at Little Lemon Restaurant."
         />
+        <meta name="og:title" content="Little Lemon Restauranr" />
+        <meta name="og:description" content="Italian Mediteranean Restaurant" />
+        <meta name="og:image" content="/logo2.png" />
         <meta name="viewport" content="width=device-width, inital-scale=1.0" />
         <link />
       </Head>

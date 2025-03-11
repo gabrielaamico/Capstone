ada6a615a8cd31434710d8500aef0ea5d605b5ad
Gabriela Mico <gabriela.mico3@gmail.com>
2025-03-11 14:21:03 +0200

    Structure of the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,84 @@
+import React from "react";
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Text
-    </div>
+    <>
+      {/* Metadata */}
+      <Head>
+        <title>Little Lemon Restaurant</title>
+        <meta
+          name="description"
+          content="Enjoy delicious italian mediteranean meals at Little Lemon Restaurant."
+        />
+        <meta name="viewport" content="width=device-width, inital-scale=1.0" />
+        <link />
+      </Head>
+
+      {/* Header with Navigation */}
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#menu">Menu</a>
+            </li>
+            <li>
+              <a href="#reservations">Reservations</a>
+            </li>
+            <li>
+              <a href="#order">Order online</a>
+            </li>
+            <li>
+              <a href="#login">Login</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      {/* Hero Section */}
+      <section id="hero">
+        <h1>Little Lemon</h1>
+        <h2>Madrid</h2>
+        <p>...</p>
+        <button>Reserve a Table</button>
+      </section>
+
+      {/* Highlights */}
+      <section id="highlights">
+        <h2>Specials</h2>
+        <button>Online Menu</button>
+        <article>
+          <h3>Greek salad 12.99$</h3>
+          <p>description</p>
+        </article>
+      </section>
+
+      {/* Testimonials */}
+      <section id="testimonials">
+        <h2>Testimonials</h2>
+        <blockquote>
+          <p>"Great food"</p>
+          <cite>Happy customer</cite>
+        </blockquote>
+      </section>
+
+      {/* About */}
+      <section id="about">
+        <h1>Little Lemon</h1>
+        <h2>Madrid</h2>
+        <p>description</p>
+      </section>
+
+      {/* Footer */}
+      <footer>
+        <p>&copy; 2025 Little Lemon. All rights reserved.</p>
+      </footer>
+    </>
   );
 }
